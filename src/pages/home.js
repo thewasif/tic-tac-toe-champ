b8207210dcee3fad7341be8fb52749732ac493ae
MuhammadWasif <wasif33@outlook.com>
2021-03-08 21:37:12 +0500

    initial setup

---
+++ src/pages/home.js
@@ -0,0 +1,14 @@
+function home() {
+  return (
+    <div className='home'>
+      <h1 className='home__title'>Tic Tac Toe Champ</h1>
+      <div className='home__btn-group'>
+        <button>Create a New Room</button>
+        <button>Join a Room</button>
+        <button>Play Offline </button>
+      </div>
+    </div>
+  );
+}
+
+export default home;
